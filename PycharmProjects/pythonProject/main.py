4f12ca58694ae8f6f5aa29bc48d02438fbd1b802
Ronny Ohawa <ochiengohawa@gmail.com>
2021-03-20 17:52:37 +0300

    python first commit :octocat:

---
+++ PycharmProjects/pythonProject/main.py
@@ -0,0 +1,7 @@
+password=int(input("please enter your password"))
+if password<1545:
+    print("wrong password")
+elif password==1545:
+    print("welcome to your web page")
+elif password>1545:
+    print("wromg password")
